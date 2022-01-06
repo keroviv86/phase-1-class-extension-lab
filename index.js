407c0d71bd8bed540f1a9fa2476f250810b2ec16
Vivian Yim <keroviv86@gmail.com>
2022-01-05 23:15:30 -0600

    commit tree 264976a23b1f050c2720196be07ca612c0189269

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+class Polygon{
+    constructor(array){
+        this.array = array
+    }
+    get countSides(){
+        return this.array.length
+    }
+
+    get perimeter(){
+        return this.array.reduce((a,b)=>a+b)
+    }
+}
+
+// class Triangle extends{
+
+// }
